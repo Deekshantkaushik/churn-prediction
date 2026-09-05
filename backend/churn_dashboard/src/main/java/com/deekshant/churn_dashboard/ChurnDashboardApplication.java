140d3fb02849edc0ac604a7700dff79f563c5341
Deekshant Kaushik <your-email@example.com>
2026-09-05 11:13:10 +0530

    Initialize Spring Boot backend with hello endpoint, MySQL config, and temporary security bypass

---
+++ backend/churn_dashboard/src/main/java/com/deekshant/churn_dashboard/ChurnDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.deekshant.churn_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurnDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurnDashboardApplication.class, args);
+	}
+
+}
