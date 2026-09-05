51619fa9354b296f89764e8b9b8a60554be0413c
Deekshant Kaushik <your-email@example.com>
2026-09-05 11:13:10 +0530

    Initialize Spring Boot backend with hello endpoint, MySQL config, and temporary security bypass

---
+++ backend/churn_dashboard/src/main/java/com/deekshant/churn_dashboard/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.deekshant.churn_dashboard.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Churn Dashboard backend is running!";
+    }
+}
+
---
+++ backend/churn_dashboard/src/test/java/com/deekshant/churn_dashboard/ChurnDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deekshant.churn_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurnDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
